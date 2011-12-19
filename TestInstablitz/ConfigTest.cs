e1eae0f396bcfdb9306857bc35b884d4696aeb0b
Bro Gramming <gramming.bro@gmail.com>
2011-12-19 15:00:17 +0100

    add test files

---
+++ TestInstablitz/ConfigTest.cs
@@ -0,0 +1,78 @@
+﻿using InstaBlitz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestInstablitz
+{
+    
+    
+    /// <summary>
+    ///Dies ist eine Testklasse für "ConfigTest" und soll
+    ///alle ConfigTest Komponententests enthalten.
+    ///</summary>
+    [TestClass()]
+    public class ConfigTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Ruft den Testkontext auf, der Informationen
+        ///über und Funktionalität für den aktuellen Testlauf bietet, oder legt diesen fest.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Zusätzliche Testattribute
+        // 
+        //Sie können beim Verfassen Ihrer Tests die folgenden zusätzlichen Attribute verwenden:
+        //
+        //Mit ClassInitialize führen Sie Code aus, bevor Sie den ersten Test in der Klasse ausführen.
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Mit ClassCleanup führen Sie Code aus, nachdem alle Tests in einer Klasse ausgeführt wurden.
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Mit TestInitialize können Sie vor jedem einzelnen Test Code ausführen.
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Mit TestCleanup können Sie nach jedem einzelnen Test Code ausführen.
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Ein Test für "Write"
+        ///</summary>
+        [TestMethod()]
+        public void WriteTest()
+        {
+            ConfigData.FILENAME = "test_file";
+            Config.Write();
+            Assert.Inconclusive("Eine Methode, die keinen Wert zurückgibt, kann nicht überprüft werden.");
+        }
+    }
+}
