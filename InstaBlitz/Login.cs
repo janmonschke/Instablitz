099ca1e12499b9567ce4a4469defa539f1a4c770
Bro Gramming <gramming.bro@gmail.com>
2011-12-19 12:17:06 +0100

    Add Register Link

--- InstaBlitz/Login.cs
+++ InstaBlitz/Login.cs
@@ -27,7 +27,7 @@ namespace InstaBlitz
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-
+            System.Diagnostics.Process.Start("http://www.instapaper.com/user/register");
         }
 
         private void label1_Click(object sender, EventArgs e)
