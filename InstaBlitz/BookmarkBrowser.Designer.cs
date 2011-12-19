edddaad6cab32457313b3b48e6e59bf64bfa0b04
Bro Gramming <gramming.bro@gmail.com>
2011-12-19 16:38:16 +0100

    Add BookmarkBrowser View

---
+++ InstaBlitz/BookmarkBrowser.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace InstaBlitz
+{
+    partial class BookmarkBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BookmarkBrowser));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.FolderList = new System.Windows.Forms.ListView();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.BookmarkList = new System.Windows.Forms.ListView();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.BookmarkView = new System.Windows.Forms.WebBrowser();
+            this.LikeBookmarkButton = new System.Windows.Forms.Button();
+            this.DeleteBookmarkButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.FolderList);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(877, 436);
+            this.splitContainer1.SplitterDistance = 133;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // FolderList
+            // 
+            this.FolderList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FolderList.Location = new System.Drawing.Point(0, 0);
+            this.FolderList.Name = "FolderList";
+            this.FolderList.Size = new System.Drawing.Size(133, 436);
+            this.FolderList.TabIndex = 0;
+            this.FolderList.UseCompatibleStateImageBehavior = false;
+            this.FolderList.View = System.Windows.Forms.View.List;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.BookmarkList);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(740, 436);
+            this.splitContainer2.SplitterDistance = 256;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // BookmarkList
+            // 
+            this.BookmarkList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BookmarkList.Location = new System.Drawing.Point(0, 0);
+            this.BookmarkList.Name = "BookmarkList";
+            this.BookmarkList.Size = new System.Drawing.Size(256, 436);
+            this.BookmarkList.TabIndex = 0;
+            this.BookmarkList.UseCompatibleStateImageBehavior = false;
+            this.BookmarkList.View = System.Windows.Forms.View.List;
+            this.BookmarkList.SelectedIndexChanged += new System.EventHandler(this.BookmarkList_SelectedIndexChanged);
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.BookmarkView);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.LikeBookmarkButton);
+            this.splitContainer3.Panel2.Controls.Add(this.DeleteBookmarkButton);
+            this.splitContainer3.Size = new System.Drawing.Size(480, 436);
+            this.splitContainer3.SplitterDistance = 407;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // BookmarkView
+            // 
+            this.BookmarkView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BookmarkView.Location = new System.Drawing.Point(0, 0);
+            this.BookmarkView.MinimumSize = new System.Drawing.Size(20, 20);
+            this.BookmarkView.Name = "BookmarkView";
+            this.BookmarkView.Size = new System.Drawing.Size(480, 407);
+            this.BookmarkView.TabIndex = 2;
+            // 
+            // LikeBookmarkButton
+            // 
+            this.LikeBookmarkButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.LikeBookmarkButton.Location = new System.Drawing.Point(405, 0);
+            this.LikeBookmarkButton.Name = "LikeBookmarkButton";
+            this.LikeBookmarkButton.Size = new System.Drawing.Size(75, 25);
+            this.LikeBookmarkButton.TabIndex = 1;
+            this.LikeBookmarkButton.Text = "Like";
+            this.LikeBookmarkButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteBookmarkButton
+            // 
+            this.DeleteBookmarkButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.DeleteBookmarkButton.Location = new System.Drawing.Point(0, 0);
+            this.DeleteBookmarkButton.Name = "DeleteBookmarkButton";
+            this.DeleteBookmarkButton.Size = new System.Drawing.Size(75, 25);
+            this.DeleteBookmarkButton.TabIndex = 0;
+            this.DeleteBookmarkButton.Text = "Delete";
+            this.DeleteBookmarkButton.UseVisualStyleBackColor = true;
+            // 
+            // BookmarkBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 436);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BookmarkBrowser";
+            this.Text = "BookmarkBrowser";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ListView FolderList;
+        private System.Windows.Forms.ListView BookmarkList;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.WebBrowser BookmarkView;
+        private System.Windows.Forms.Button LikeBookmarkButton;
+        private System.Windows.Forms.Button DeleteBookmarkButton;
+
+    }
+}
---
+++ InstaBlitz/BookmarkBrowser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InstaBlitz
+{
+    public partial class BookmarkBrowser : Form
+    {
+        public BookmarkBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BookmarkList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
